d129a6066c78001d9593a573b87c4f7e5d5b6c00
3060s <78622374+3060s@users.noreply.github.com>
2023-11-17 18:23:02 +0100

    Kupowanie pracowników

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -35,13 +36,19 @@
             this.UpgradeButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.pracownik1 = new System.Windows.Forms.Button();
+            this.pracownik2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Timer2 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(291, 101);
+            this.pictureBox1.Location = new System.Drawing.Point(541, 139);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(204, 128);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -53,7 +60,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Rubik", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(321, 254);
+            this.label1.Location = new System.Drawing.Point(570, 280);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(146, 28);
             this.label1.TabIndex = 1;
@@ -64,7 +71,7 @@
             // 
             this.ScoreLabel.AutoSize = true;
             this.ScoreLabel.Font = new System.Drawing.Font("Rubik", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.ScoreLabel.Location = new System.Drawing.Point(382, 327);
+            this.ScoreLabel.Location = new System.Drawing.Point(636, 341);
             this.ScoreLabel.Name = "ScoreLabel";
             this.ScoreLabel.Size = new System.Drawing.Size(29, 28);
             this.ScoreLabel.TabIndex = 2;
@@ -73,7 +80,7 @@
             // 
             // UpgradeButton
             // 
-            this.UpgradeButton.Location = new System.Drawing.Point(326, 403);
+            this.UpgradeButton.Location = new System.Drawing.Point(575, 413);
             this.UpgradeButton.Name = "UpgradeButton";
             this.UpgradeButton.Size = new System.Drawing.Size(141, 23);
             this.UpgradeButton.TabIndex = 3;
@@ -92,19 +99,71 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(536, 399);
+            this.label3.Location = new System.Drawing.Point(822, 413);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(76, 25);
             this.label3.TabIndex = 4;
             this.label3.Text = "label3";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
+            // pracownik1
+            // 
+            this.pracownik1.Location = new System.Drawing.Point(1097, 139);
+            this.pracownik1.Name = "pracownik1";
+            this.pracownik1.Size = new System.Drawing.Size(75, 23);
+            this.pracownik1.TabIndex = 5;
+            this.pracownik1.Text = "Pracownik 1";
+            this.pracownik1.UseVisualStyleBackColor = true;
+            this.pracownik1.Click += new System.EventHandler(this.pracownik1_Click);
+            // 
+            // pracownik2
+            // 
+            this.pracownik2.Location = new System.Drawing.Point(1097, 224);
+            this.pracownik2.Name = "pracownik2";
+            this.pracownik2.Size = new System.Drawing.Size(75, 23);
+            this.pracownik2.TabIndex = 6;
+            this.pracownik2.Text = "Pracownik 2";
+            this.pracownik2.UseVisualStyleBackColor = true;
+            this.pracownik2.Click += new System.EventHandler(this.pracownik2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(797, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(265, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Cena zakupu: 100 serów";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(797, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(268, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Cena zakupu: 300 serów";
+            // 
+            // Timer1
+            // 
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick_1);
+            // 
+            // Timer2
+            // 
+            this.Timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1261, 692);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pracownik2);
+            this.Controls.Add(this.pracownik1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.UpgradeButton);
@@ -128,6 +187,12 @@
         private System.Windows.Forms.Button UpgradeButton;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button pracownik1;
+        private System.Windows.Forms.Button pracownik2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.Timer Timer2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,8 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace Clicker_game
 {
@@ -17,6 +20,9 @@ namespace Clicker_game
 
         int upgrade_level = 1;
         int upgrade_price = 10;
+        int pracownik1_cena = 100;
+        int pracownik2_cena = 300;
+
 
         public Form1()
         {
@@ -61,6 +67,50 @@ namespace Clicker_game
         private void label3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void pracownik1_Click(object sender, EventArgs e)
+        {
+            if (pracownik1_cena < score)
+            {
+                pracownik1.Visible = false;
+                label4.Hide();
+                score -= pracownik1_cena;
+                ScoreLabel.Text = score.ToString();
+                Timer1.Interval = 250;
+                Timer1.Start();
+            }
+        }
+
+        private void pracownik2_Click(object sender, EventArgs e)
+        {
+            if (pracownik2_cena < score)
+            {
+                pracownik2.Visible = false;
+                label5.Hide();
+                score -= pracownik2_cena;
+                ScoreLabel.Text = score.ToString();
+                Timer2.Interval = 250;
+                Timer2.Start();
+            }
+
+
+
+        }
+
+
+        private void Timer1_Tick_1(object sender, EventArgs e)
+        {
+            score++;
+            ScoreLabel.Text = score.ToString();
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            score++;
+            score++;
+            score++;
+            ScoreLabel.Text = score.ToString();
         }
     }
 }
