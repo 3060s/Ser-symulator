a3e6b48c29095ba614f27e03e11999ac3b03fd04
RD3V <47334312+sirdx@users.noreply.github.com>
2023-11-19 12:16:50 +0100

    Podstawa pod ulepszenia pracowników

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -61,24 +61,22 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Rubik", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.Location = new System.Drawing.Point(570, 280);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(146, 28);
+            this.label1.Size = new System.Drawing.Size(148, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "Klikaj w ser\r\n";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // ScoreLabel
             // 
             this.ScoreLabel.AutoSize = true;
-            this.ScoreLabel.Font = new System.Drawing.Font("Rubik", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.ScoreLabel.Location = new System.Drawing.Point(636, 341);
             this.ScoreLabel.Name = "ScoreLabel";
-            this.ScoreLabel.Size = new System.Drawing.Size(29, 28);
+            this.ScoreLabel.Size = new System.Drawing.Size(27, 29);
             this.ScoreLabel.TabIndex = 2;
             this.ScoreLabel.Text = "0";
-            this.ScoreLabel.Click += new System.EventHandler(this.ScoreLabel_Click);
             // 
             // UpgradeButton
             // 
@@ -100,13 +98,12 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label3.Location = new System.Drawing.Point(822, 413);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(76, 25);
             this.label3.TabIndex = 4;
             this.label3.Text = "label3";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // pracownik1
             // 
@@ -131,26 +128,26 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label4.Location = new System.Drawing.Point(797, 137);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(265, 25);
+            this.label4.Size = new System.Drawing.Size(272, 25);
             this.label4.TabIndex = 7;
             this.label4.Text = "Cena zakupu: 100 serów";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Rubik", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label5.Location = new System.Drawing.Point(797, 220);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(268, 25);
+            this.label5.Size = new System.Drawing.Size(272, 25);
             this.label5.TabIndex = 8;
             this.label5.Text = "Cena zakupu: 300 serów";
             // 
             // Timer1
             // 
-            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick_1);
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
             // Timer2
             // 
@@ -164,7 +161,7 @@
             this.Ulepszenie1.TabIndex = 9;
             this.Ulepszenie1.Text = "Ulepszenie1";
             this.Ulepszenie1.UseVisualStyleBackColor = true;
-            this.Ulepszenie1.Click += new System.EventHandler(this.Ulepszenie1_Click);
+            this.Ulepszenie1.Visible = false;
             // 
             // Ulepszenie2
             // 
@@ -174,6 +171,7 @@
             this.Ulepszenie2.TabIndex = 10;
             this.Ulepszenie2.Text = "Ulepszenie2";
             this.Ulepszenie2.UseVisualStyleBackColor = true;
+            this.Ulepszenie2.Visible = false;
             // 
             // Form1
             // 
@@ -195,7 +193,6 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
